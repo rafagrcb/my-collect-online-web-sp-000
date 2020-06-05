680d8fd8beaf466d0cd07f30e1824786e2331c48
Rafael Garcia <rafagrcb@gmail.com>
2020-06-05 18:49:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,10 @@
-
+array = ["Tim Jones", "Tom Smith", "Sophie Ruiz", "Antoin Chavez"]
 
 def my_collect(array)
   i = 0
   name_collection = []
   while i < array.length
     name_collection.push yield(array[i])
-  # you could also do it this way:
-  # name_collection << yield(array[i])
     i += 1
   end
   name_collection
