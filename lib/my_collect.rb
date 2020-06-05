af5bcebd0d11338332955f23453c0d0d4dd39047
Rafael Garcia <rafagrcb@gmail.com>
2020-06-05 18:41:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,7 @@
 languages = ["ruby", "javascript", "python", "objective-c"]
 
-def my_collect(array)
+array = ["Tim Jones", "Tom Smith", "Sophie Ruiz", "Antoin Chavez"]
+
+def my_collect(array) do |array|
+  array.split(" ").first
 end
