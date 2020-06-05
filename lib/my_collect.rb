aa54ab606a6ffadae111c9cd0669a8fbd597f77c
Rafael Garcia <rafagrcb@gmail.com>
2020-06-05 18:49:40 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+array = ["Tim Jones", "Tom Smith", "Sophie Ruiz", "Antoin Chavez"]
 
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+    i += 1
+  end
+  name_collection
+end
 
+my_collect(array) {|i| i.split(" ").first}
